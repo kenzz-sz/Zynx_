24e5af8921110b51e575f8acab4b32d7fd89d6fb
kenzz-sz <r4mseiandsei@gmail.com>
2026-04-27 19:57:23 +0700

    update

---
+++ main/mods/list.js
@@ -0,0 +1,6 @@
+[
+ {"directory":"run-js",
+ "descripsion":"in this mod you can run js code",
+ "display":"run javascript",
+ "code":`createscene('game', 'game menu', '', 'gamev', 'dashboard', true)`}
+]
