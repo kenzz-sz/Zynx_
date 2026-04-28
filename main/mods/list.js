40ffb54e0c6c551daa4874b37662f5fda3c7bf54
kenzz-sz <r4mseiandsei@gmail.com>
2026-04-28 08:06:11 +0700

    update list.js

--- main/mods/list.js
+++ main/mods/list.js
@@ -1,6 +1,6 @@
 resultfetch = [
- {"directory":"run-js",
- "descripsion":"in-this-mod-you-can-run-js-code",
- "display":"run-javascript",
- "code":"alert('work?')"}
+ {directory:'run-js',
+ descripsion:'in-this-mod-you-can-run-js-code',
+ display:'run-javascript',
+ code:'alert(\'work?\')'}
 ]
