f530f4006a9cb55bef2da65ae1fbd80407271eda
kenzz-sz <r4mseiandsei@gmail.com>
2026-04-29 10:35:11 +0700

    Update tlsitem.js

--- main/mods/item/tlsitem.js
+++ main/mods/item/tlsitem.js
@@ -1,1 +1,16 @@
-rmfs("https://raw.githubusercontent.com/kenzz-sz/Zynx_/refs/heads/main/main/mods/item/locked.json")
+//rmfs("https://raw.githubusercontent.com/kenzz-sz/Zynx_/refs/heads/main/main/mods/item/locked.json")
+
+let sampletlsa = [
+{
+'d': 'eval(',
+'type': 'ask',
+'text': 'This mods use eval(), this will be dangerous, continue?'
+},
+{'d': ('document'+'.body'),
+'type': 'full',
+'text': ('This mods want to looking to document' + '.body')
+}
+];
+
+deniedmodsitem = sampletlsa;
+allowpushdeniedmods = false;
